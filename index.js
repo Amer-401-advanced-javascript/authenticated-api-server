a1e89d91fd3ef2f64344744e405304aa3a2c5222
Amer-bit <ny.1995@outlook.com>
2020-07-14 23:08:06 +0300

    Working Server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const server = require('./lib/server');
 const MONGODB_URI = 'mongodb://localhost:27017/authenticated-api-server';
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(MONGODB_URI || process.env.MONGODB_URI, {
   useFindAndModify:false,
   useCreateIndex: true , 
   useUnifiedTopology: true,
